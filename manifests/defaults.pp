b6fc877fc2b50993d67b7504b9c29a86d129f41d
Stefan van Wouw <stefanvanwouw@gmail.com>
2014-03-07 15:33:30 +0000

    Upgrade to Impala 1.2.3

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -2,5 +2,5 @@ class impala::defaults {
     $impala_state_store_host = 'localhost'
     $impala_state_store_port = 24000
     $impala_backend_port = 22000
-    $release = 'precise-impala1.1.1'
+    $release = 'precise-impala1.2.3'
 }
